b1b9bbff98b65aa6a1b175b6efa89c3341ad9431
Elliot <16152037+LluisMas@users.noreply.github.com>
2019-11-05 19:35:37 +0100

    Implement Sidebar

--- bookify/src/app/app.module.ts
+++ bookify/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AdminSidebarComponent } from './admin/admin-sidebar/admin-sidebar.component';
