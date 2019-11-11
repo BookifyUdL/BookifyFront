702ad4dd65e626f48fa4e0e42a3c4281bdd44f4a
Elliot <16152037+LluisMas@users.noreply.github.com>
2019-11-11 20:33:49 +0100

    Admin Achievements - Done daddy

--- bookify/src/app/app-routing.module.ts
+++ bookify/src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import {AdminGenresComponent} from './admin/admin-genres/admin-genres.component';
-import {AdminMainComponent} from './admin/admin-main/admin-main.component';
+import { AdminGenresComponent } from './admin/admin-genres/admin-genres.component';
+import { AdminMainComponent } from './admin/admin-main/admin-main.component';
+import { AdminAchievementsComponent } from './admin/admin-achievements/admin-achievements.component';
 
 
 const routes: Routes = [
   { path: 'admin', component: AdminMainComponent },
   { path: 'admin/genres', component: AdminGenresComponent },
+  { path: 'admin/achievements', component: AdminAchievementsComponent },
 ];
 
 @NgModule({
